063608ace70c46b03468fbbbffa017489afdf690
Uri Rosenberg <ur9530@intl.att.com>
2018-06-17 09:59:02 +0300

    added something to sub-a

--- build/detect-changes.sh
+++ build/detect-changes.sh
@@ -4,6 +4,7 @@ export IGNORE_FILES=$(ls -p | grep -v /)
 detect_changed_folders() {
 	echo "detecting changes for this build"
 	folders=`git diff --name-only $SHIPPABLE_COMMIT_RANGE | sort -u | awk 'BEGIN {FS="/"} {print $1}' | uniq`
+ 	echo "changes in $folders"
  	export changed_components=$folders
 }
 
